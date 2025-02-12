b184423477ac2252c343d22753282b21015eacee
natthawutphetmat <167544607+natthawutphetmat@users.noreply.github.com>
2025-02-13 01:51:00 +0700

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,7 +11,24 @@ export default function page() {
 
   return (
     <>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>PG-V9 | PG-Phoneโทรศัพท์มือถือราคาถูกที่สุด PG-V9 PRO </title>
+        <meta name="description" content="PG-V9 | PG-Phoneโทรศัพท์มือถือราคาถูกที่สุด สมาร์ทโฟนรุ่นใหม่ล่าสุดPG-V9 PRO ความกว้างหน้าจอ6นิ้ว ความละเอียดหน้าจอHD"/>
+        <meta name="keywords" content="มือถือ, สมาร์ทโฟน, ร้านขายมือถือ, ราคามือถือ, สั่งซื้อมือถือออนไลน์, ผ่อนมือถือ"/>
+        <meta property="og:title" content="PG-V9 | PG-Phoneโทรศัพท์มือถือราคาถูกที่สุด" />
+        <meta property="og:description" content="PG-V9 | PG-Phoneโทรศัพท์มือถือราคาถูกที่สุด สมาร์ทโฟนรุ่นใหม่ล่าสุดPG-V9 PRO ความกว้างหน้าจอ6นิ้ว ความละเอียดหน้าจอHD"/>
+        <meta name="robots" content="index, follow" />
+        <meta property="og:type" content="website" />
 
+        <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon" />
+      
+        <meta property="og:image:type" content="image/jpeg"/>
+        <meta property="og:image:height" content="2000"/>
+        <meta property="og:image:width" content="2000"/>
+        <meta property="og:image" content="https://www.mypgphone.com/img/phone-1.jpg"/>
+
+      
 
     <Header/>
     <Videos/>
