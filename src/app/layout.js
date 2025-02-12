78620f168146e6a44a71c3a02ae30e550d08127d
natthawutphetmat <167544607+natthawutphetmat@users.noreply.github.com>
2025-02-13 01:50:28 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,7 +26,6 @@ export default function RootLayout({ children }) {
         <meta name="keywords" content="มือถือ, สมาร์ทโฟน, ร้านขายมือถือ, ราคามือถือ, สั่งซื้อมือถือออนไลน์, ผ่อนมือถือ"/>
         <meta property="og:title" content="PG-V9 | PG-Phoneโทรศัพท์มือถือราคาถูกที่สุด" />
         <meta property="og:description" content="PG-V9 | PG-Phoneโทรศัพท์มือถือราคาถูกที่สุด สมาร์ทโฟนรุ่นใหม่ล่าสุดPG-V9 PRO ความกว้างหน้าจอ6นิ้ว ความละเอียดหน้าจอHD"/>
-        <meta property="og:image" content="/img/phone-1.jpg"/>
         <meta name="robots" content="index, follow" />
         <meta property="og:type" content="website" />
 
@@ -35,13 +34,7 @@ export default function RootLayout({ children }) {
         <meta property="og:image:type" content="image/jpeg"/>
         <meta property="og:image:height" content="2000"/>
         <meta property="og:image:width" content="2000"/>
-        <meta property="og:image" content="/img/phone-1.jpg"/>
-
-
-
-
-
-
+        <meta property="og:image" content="https://www.mypgphone.com/img/phone-1.jpg"/>
 
         <Script
           strategy="afterInteractive"
